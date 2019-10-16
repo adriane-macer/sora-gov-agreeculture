f4d6ae469edc155d116c6646042d613fe3646e62
Adrian Evangelista <adriane.macer@gmail.com>
2019-10-16 10:56:22 +0800

    added part of service model.

---
+++ sora_gov_agree/lib/models/service.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'service.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Service _$ServiceFromJson(Map<String, dynamic> json) {
+  return Service();
+}
+
+Map<String, dynamic> _$ServiceToJson(Service instance) => <String, dynamic>{};
